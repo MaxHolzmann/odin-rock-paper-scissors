5224ecb68f67d1830c9cf8870741b7d760aa77c4
MaxHolzmann <max@holzmann.io>
2022-07-04 11:55:11 -0500

    begins logic of game

--- script.js
+++ script.js
@@ -10,7 +10,40 @@ playRound function for user
 Make input non case-sensitive
 playRound has two parameters, playerSelection and computerSelection.
 
+-- LOGIC --
+Rock beats Scissors
+Paper beats Rock
+Scissors Beats paper
+
 After a single game, code for multiple games using a for loop.
 I will begin this after finishing a single game. 
 
 */ 
+
+const getRandomInt = () => {
+    min = 0;
+    max = 3;
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+const computerPlay = () => {
+ 
+    // create this logic somewhere else 
+   const computerTurn = getRandomInt();
+    console.log(computerTurn);
+   if(computerTurn == 0) {
+    console.log("rock")
+   } else if (computerTurn == 1) {
+    console.log("paper")
+   } else if (computerTurn == 2) {
+    console.log("scissors")
+   }
+}
+
+const playRound = () => {
+    const computerSelection = getRandomInt();
+    let playerSelection = prompt("Rock, Paper or Scissors?");
+    //create a function to look at the strings.
+    let playerValue = playerSelection.toLowerCase();
+    console.log(playerValue);
+}
