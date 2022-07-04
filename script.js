8fee86609a238788f94e160c3e40eb4bd014e116
MaxHolzmann <max@holzmann.io>
2022-07-03 20:43:15 -0500

    Sets up initial files and lays out psuedo code for rps game

---
+++ script.js
@@ -0,0 +1,16 @@
+// Thinking through the game! 
+
+/* 
+
+Played against a Computer
+Computer function will be called computerPlay()
+computerPlay will return a randomly generated string, Rock/Paper/Scissors
+
+playRound function for user
+Make input non case-sensitive
+playRound has two parameters, playerSelection and computerSelection.
+
+After a single game, code for multiple games using a for loop.
+I will begin this after finishing a single game. 
+
+*/ 
